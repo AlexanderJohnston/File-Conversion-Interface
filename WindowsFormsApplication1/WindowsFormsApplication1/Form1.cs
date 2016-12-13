ff79b5cf34669f36affc284e7e899d52c0e29a7b
Alexander Johnston <alexanderj.roaming@gmail.com>
2016-12-13 13:19:07 -0500

    I have added the password hashing classes.

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,10 +26,18 @@ namespace MainControl
     {
         public static void Start()
         {
+            string testVariable = null;
             string userName = null;
             string userFormCaption = "Login";
-            string userFormText = "Please enter your first and last name to log in.";
+            string userFormText = "Welcome to the file conversion interface." + '\n' + "Please enter your first and last name.";
             userName = Prompt.ShowDialog(userFormText, userFormCaption);
+
+            string userPassword = null;
+            MD5 md5Hash = MD5.Create();
+            userFormCaption = "Password";
+            userFormText = "Please enter your password.";
+            userPassword = Prompt.ShowDialog(userFormText, userFormCaption);
+            byte[] finalPassword = PasswordManagement.HashPassword(userPassword, md5Hash);
         }
     }
 
@@ -57,5 +66,138 @@ namespace MainControl
             return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
         }
     }
+
+    /* http://www.dreamincode.net/forums/topic/196519-basic-login-system-part-i-password-handling/
+     This document is used for the password management. I'm saving this link for personal study later on.
+    */
+    public static class PasswordManagement
+    {
+        /// <summary>
+        /// Creates a byte array containing the hashed combination of
+        /// a GUID-generated salt value prepended to the password, hashed
+        /// according to the provided algorithm.
+        /// </summary>
+        /// <param name="password">The plaintext password to be hashed</param>
+        /// <param name="hashAlg">The HashAlgorithm to use in hashing the data</param>
+        /// <returns>A byte array of the concatenated salt &
+        /// hashed salt/password combination</returns>
+        public static byte[] HashPassword(string password, HashAlgorithm hashAlg)
+        {
+            // Convert the password to a byte array
+            byte[] passwordAsByteArray = UTF8Encoding.UTF8.GetBytes(password);
+
+            // Generate a salt for the password
+            byte[] saltAsByteArray = Guid.NewGuid().ToByteArray();
+
+            // Hash the salt/password combination
+            byte[] hashedPassAndSalt = HashPassword( 
+                saltAsByteArray, passwordAsByteArray, hashAlg);
+
+            // Prepend the salt to the hashed data
+            byte[] finalOutput = new byte[saltAsByteArray.Length + hashedPassAndSalt.Length];
+            Array.Copy(saltAsByteArray, finalOutput, saltAsByteArray.Length);
+            Array.Copy(hashedPassAndSalt, 0, finalOutput,
+                saltAsByteArray.Length, hashedPassAndSalt.Length);
+
+            return finalOutput;
+        }
+
+        /// <summary>
+        /// Prepends the provided salt to the provided password using the provided
+        /// HashAlgorithm
+        /// </summary>
+        /// <param name="salt">The salt to prepend</param>
+        /// <param name="password">The password</param>
+        /// <param name="hashAlg">The HashAlgorithm to use</param>
+        /// <returns>A byte array containing the hashed salt/password combination.</returns>
+        private static byte[] HashPassword(byte[] salt, byte[] password, HashAlgorithm hashAlg)
+        {
+            // Combine the salt and password into a single byte array
+            byte[] passAndSaltForHashing = new byte[salt.Length + password.Length];
+            Array.Copy(salt, passAndSaltForHashing, salt.Length);
+            Array.Copy(password, 0, passAndSaltForHashing,
+                salt.Length, password.Length);
+
+            // Hash the salt/password combination
+            return hashAlg.ComputeHash(passAndSaltForHashing);
+        }   
+
+        /// <summary>
+        /// Compares a plaintext password provided by the user to the
+        /// password stored in a hashed salt/password byte array.
+        /// </summary>
+        /// <param name="password">The user-provided plaintext password for comparison</param>
+        /// <param name="storedPassAndSalt">The stored hashed salt/password combination</param>
+        /// <param name="hashAlg">The hash algorithm to use</param>
+        /// <returns>True if the password provided matches the stored pass, otherwise
+        /// false.</returns>
+        public static bool ComparePassword(string password,
+            byte[] storedPassAndSalt, HashAlgorithm hashAlg)
+        {
+            // Get salt from start of storedPassAndSalt
+            int hashSize = hashAlg.HashSize / 8;
+
+            // Deduce the size of the salt from the hash length
+            int saltSize = storedPassAndSalt.Length - hashSize;
+
+            // Extract salt from storedPassAndSalt
+            byte[] salt = new byte[storedPassAndSalt.Length - hashSize];
+            Array.Copy(storedPassAndSalt, salt, saltSize);
+
+            // Extract hash from storedPassAndSalt
+            byte[] hashedPasswordFromFile = new byte[storedPassAndSalt.Length - salt.Length];
+            Array.Copy(storedPassAndSalt, salt.Length, hashedPasswordFromFile, 0, hashSize);
+
+            // Using the salt extracted from the storeed password,
+            // hash the password we received from the user.
+            byte[] hashedPasswordFromUser = HashPassword(
+                salt, UTF8Encoding.UTF8.GetBytes(password), hashAlg);
+
+            // Compare the stored and provided hashes
+            return hashedPasswordFromFile.SequenceEqual(hashedPasswordFromUser);
+        }
+
+        /// <summary>
+        /// Create a human-readable hexadecimal string from the
+        /// byte array by walking the array and converting each byte
+        /// into a 2-digit hexadecimal value.
+        /// </summary>
+        /// <param name="data">The byte array to make human-readable</param>
+        /// <returns>The human-readable string</returns>
+        public static string CreateTextString(byte[] data)
+        {
+            // Create a human-readable hexadecimal string from the
+            // byte array by walking the array and converting each byte
+            // into a 2-digit hexadecimal value.
+            StringBuilder sb = new StringBuilder(data.Length * 2);
+            for (int i = 0; i < data.Length; ++i)
+            {
+                sb.AppendFormat("{0:x2}", data[i]);
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Transform the provided human-readable hexadecimal string to
+        /// an array of bytes.
+        /// </summary>
+        /// <param name="data">The string to transform</param>
+        /// <returns>The byte array representation of the hexadecimal string</returns>
+        public static byte[] CreateByteArray(string data)
+        {
+            // Since each byte is represented by a 2-digit hex number,
+            // we know that the length of the resulting byte array is
+            // half the length of the passed-in data.
+            byte[] binData = new byte[data.Length / 2];
+            for (int i = 0; i < data.Length; i += 2)
+            {
+                binData[i / 2] = Convert.ToByte(data.Substring(i, 2), 16);
+            }
+
+            return binData;
+        }
+    }
+
+
 
 }
