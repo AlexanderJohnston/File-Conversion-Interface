7ad79d04838c49ecb627d557387bc3324119d704
Alexander Johnston <alexanderj.roaming@gmail.com>
2016-12-22 09:50:05 -0500

    Added ampersand to the hotkey letters of each button.

--- DataConversionInterface/Conversion Interface/MainForm.Designer.cs
+++ DataConversionInterface/Conversion Interface/MainForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridViewGeneral = new System.Windows.Forms.DataGridView();
             this.conversionTablesList = new System.Windows.Forms.ComboBox();
             this.tablesListLabel = new System.Windows.Forms.Label();
@@ -67,14 +67,14 @@
             this.dataGridViewGeneral.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewGeneral.Location = new System.Drawing.Point(12, 32);
             this.dataGridViewGeneral.Name = "dataGridViewGeneral";
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridViewGeneral.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewGeneral.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridViewGeneral.RowTemplate.ReadOnly = true;
             this.dataGridViewGeneral.Size = new System.Drawing.Size(1107, 205);
             this.dataGridViewGeneral.TabIndex = 0;
@@ -131,7 +131,7 @@
             this.buttonOpenDataFile.Name = "buttonOpenDataFile";
             this.buttonOpenDataFile.Size = new System.Drawing.Size(75, 23);
             this.buttonOpenDataFile.TabIndex = 7;
-            this.buttonOpenDataFile.Text = "Select File";
+            this.buttonOpenDataFile.Text = "&Select File";
             this.buttonOpenDataFile.UseVisualStyleBackColor = true;
             this.buttonOpenDataFile.Click += new System.EventHandler(this.buttonOpenDataFile_Click);
             // 
@@ -141,7 +141,7 @@
             this.buttonLoadDataFile.Name = "buttonLoadDataFile";
             this.buttonLoadDataFile.Size = new System.Drawing.Size(75, 23);
             this.buttonLoadDataFile.TabIndex = 8;
-            this.buttonLoadDataFile.Text = "Analyze";
+            this.buttonLoadDataFile.Text = "&Analyze";
             this.buttonLoadDataFile.UseVisualStyleBackColor = true;
             this.buttonLoadDataFile.Click += new System.EventHandler(this.buttonLoadDataFile_Click);
             // 
@@ -181,7 +181,7 @@
             this.buttonSaveTable.Name = "buttonSaveTable";
             this.buttonSaveTable.Size = new System.Drawing.Size(75, 23);
             this.buttonSaveTable.TabIndex = 15;
-            this.buttonSaveTable.Text = "Save Table";
+            this.buttonSaveTable.Text = "Sav&e Table";
             this.buttonSaveTable.UseVisualStyleBackColor = true;
             this.buttonSaveTable.Click += new System.EventHandler(this.buttonSaveTable_Click);
             // 
